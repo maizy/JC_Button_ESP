dcda857fc2604c27c5d4f6427d3362c1e7360f9c
Nikita Kovaliov <nikita@maizy.ru>
2019-02-06 18:56:48 +0300

    fix manifest & examples

--- examples/SimpleOnOff/SimpleOnOff.ino
+++ examples/SimpleOnOff/SimpleOnOff.ino
@@ -6,7 +6,7 @@
 // Example sketch to turn an LED on and off with a tactile button switch.
 // Wire the switch from the Arduino pin to ground.
 
-#include <JC_Button.h>          // https://github.com/JChristensen/JC_Button
+#include <JC_Button_ESP8266.h>  // https://github.com/maizy/JC_Button
 
 // pin assignments
 const byte
